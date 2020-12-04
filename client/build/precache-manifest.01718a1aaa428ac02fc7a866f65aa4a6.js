1c300b069b8035b3bdfe7fba16cb98b158cb2104
Vasile <tronciu92@gmail.com>
2020-12-04 13:53:42 -0800

    deploy

---
+++ client/build/precache-manifest.01718a1aaa428ac02fc7a866f65aa4a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "71f4f4a379b1d6dabf11",
+    "url": "/static/css/main.cb4e3f9b.chunk.css"
+  },
+  {
+    "revision": "71f4f4a379b1d6dabf11",
+    "url": "/static/js/main.4e90c67c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d9eb6bb64f27f3227e15",
+    "url": "/static/js/2.fc369e8d.chunk.js"
+  },
+  {
+    "revision": "2ea5be2e4366344840aa614a3c953944",
+    "url": "/static/media/books.2ea5be2e.jpg"
+  },
+  {
+    "revision": "3be677b4a3301822e5644e7e0951322a",
+    "url": "/index.html"
+  }
+];
